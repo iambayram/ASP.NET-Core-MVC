b083f752d35717b4af382becac8706e887f8b0bb
Serhat Akköse <Serhat Akkse@DESKTOP-KDDC79A>
2023-04-16 12:38:13 +0300

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-KDDC79A\\SQLEXPRESS;Database=BulkyBook;Trusted_Connection=True;");
+            }
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
